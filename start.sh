e3d8065b3d1eac1e00275f5a83c3838abdd45259
kumarviresh25 <104244569+kumarviresh25@users.noreply.github.com>
2022-05-18 17:42:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,5 +12,4 @@ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key 
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install jenkins
-sudo systemctl status jenkins
 
