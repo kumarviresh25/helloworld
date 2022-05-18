c0c312925f7451e78f07722eb9580cbaf08af2a5
kumarviresh25 <104244569+kumarviresh25@users.noreply.github.com>
2022-05-18 17:25:38 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update
+java -version
+sudo apt install default-jre
+java -version
+sudo apt install default-jdk
+javac -version
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export PATH=$PATH:$JAVA_HOME/bin
+echo $PATH
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl status jenkins
+
