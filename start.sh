4f0ceb27f7f21feb11463b30a75319706e79ff5d
kumarviresh25 <104244569+kumarviresh25@users.noreply.github.com>
2022-05-27 14:47:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,43 +1,7 @@
 #!/bin/bash
-sudo apt install -y python-software-properties
-sudo apt install -y pkg-config
-sudo apt install -y gfortran
-sudo apt install -y libatlas-dev
-sudo apt install -y libblas-dev
-sudo apt install -y liblapack-dev
-sudo apt install -y liblapack3gf
-sudo apt install -y curl
-sudo apt install -y git
-sudo apt install -y python-virtualenv
-sudo apt install -y python-scipy
-sudo apt install -y python-numpy
-sudo apt install -y build-essential
-sudo apt install -y python-dev
-sudo apt install -y gfortran
-sudo apt install -y libfreetype6-dev
-sudo apt install -y libpng12-dev
-sudo apt install -y libjpeg-dev
-sudo apt install -y libtiff4-dev
-sudo apt install -y zlib1g-dev
-sudo apt install -y libxml2-dev
-sudo apt install -y libxslt-dev
-sudo apt install -y yui-compressor
-sudo apt install -y graphviz
-sudo apt install -y libgraphviz-dev
-sudo apt install -y graphviz-dev
-sudo apt install -y mysql-server
-sudo apt install -y libmysqlclient-dev
-sudo apt install -y libgeos-dev
-sudo apt install -y libreadline6
-sudo apt install -y libreadline6-dev
-sudo apt install -y mongodb
-sudo apt install -y nodejs
-sudo apt install -y mysql-client
-sudo apt install -y virtualenvwrapper
-sudo apt install -y libgeos-ruby1.8
-sudo apt install -y lynx-cur
-sudo apt install -y libxmlsec1-dev
-sudo apt install -y swig
+sudo apt install -y python3-pip
+pip install paver
+
 
 
 
