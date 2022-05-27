9cd8ab190a807834e9b85c901bba030792a92f4d
kumarviresh25 <104244569+kumarviresh25@users.noreply.github.com>
2022-05-27 13:02:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 sudo apt-get install -y python3-pip
 pip install -U Paver
 source /edx/app/edxapp/edxapp_env
-python3 ../manage.py lms --settings production shell
+python3 manage.py lms --settings production shell
 
 
